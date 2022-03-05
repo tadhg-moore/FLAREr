4089da50a209dafc00ade7de2130ad5f0ca4c5f4
Tadhg Moore <tadhgm@vt.edu>
2022-03-05 06:20:36 -0500

    New function for checking model output

---
+++ R/check_model_output.R
@@ -0,0 +1,67 @@
+#' @param folder
+#' @param model
+#' @noRd
+
+check_model_output <- function(folder, model) {
+
+  if(model == "GLM") {
+    outfile <- file.path(folder, model, "output", "output.nc")
+    file_check <- file.exists(outfile)
+
+    if(file_check) {
+      nc <- ncdf4::nc_open(outfile)
+      on.exit({
+        ncdf4::nc_close(nc)
+      })
+      tmp <- ncdf4::ncvar_get(nc, "temp")
+      nan_check <- all(is.nan(unlist(tmp)))
+      if(nan_check) {
+        return(FALSE)
+      } else {
+        return(TRUE)
+      }
+    } else {
+      return(FALSE)
+    }
+  } else if(model == "GOTM") {
+    outfile <- file.path(folder, model, "output", "output.nc")
+    file_check <- file.exists(outfile)
+
+    if(file_check) {
+      nc <- ncdf4::nc_open(outfile)
+      on.exit({
+        ncdf4::nc_close(nc)
+      })
+      tmp <- ncdf4::ncvar_get(nc, "temp")
+      nan_check <- all(is.nan(unlist(tmp)))
+      if(nan_check) {
+        return(FALSE)
+      } else {
+        return(TRUE)
+      }
+    } else {
+      return(FALSE)
+    }
+  }
+
+  if(model == "Simstrat") {
+    outfile <- file.path(folder, model, "output", "T_out.dat")
+    file_check <- file.exists(outfile)
+
+    if(file_check) {
+      tmp <- vroom::vroom(outfile, delim = ",", show_col_types = FALSE)
+      nan_check <- any(is.nan(unlist(tmp)))
+      if(nan_check) {
+        return(FALSE)
+      } else {
+        if(nrow(tmp) == 0) {
+          return(FALSE)
+        } else {
+          return(TRUE)
+        }
+      }
+    } else {
+      return(FALSE)
+    }
+  }
+}
